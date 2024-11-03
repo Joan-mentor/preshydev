189f84c909d6a03940ba7d8b8691c2282fd05aae
joan-mentor <preciousamii03@gmail.com>
2024-11-04 00:59:46 +0100

    Emerk project

---
+++ header.php
@@ -0,0 +1,63 @@
+<section>
+    <div class="top-bar">
+      <span><ion-icon name="call-outline"></ion-icon>+23485857393</span>
+      <ul>
+        <li>
+          <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+        </li>
+        <li>
+          <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+        </li>
+        <li>
+          <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+        </li>
+      </ul>
+    </div>
+
+    <nav>
+      <div class="logo">
+        <a href="#"
+          ><img src="images/logo.jpeg" alt="logo" /><span class="font-bold"
+            ><b class="shade">U</b>teams</span
+          ></a
+        >
+      </div>
+      <div class="toggle">
+        <a href="#"><ion-icon name="menu-outline"></ion-icon></a>
+      </div>
+      <ul class="menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">blog</a></li>
+        <li><a href="#">contacts</a></li>
+      </ul>
+    </nav>
+  </section>
+    <section class="hero">
+      <div class="home">
+        <div class="container">
+          <div class="row">
+            <div class="home-text">
+              <h1>IS ALL ABOUT</h1>
+              <p class="animate-text">
+                <span>The center of bussiness</span>
+                <span
+                  >Mission: Improving society with its own talented and skilled
+                  people</span
+                >
+                <span>Great Team The real men with real business ideas.</span>
+                <span>Studing the problem, and bringing solutions to them</span>
+                <span>The common man shall smile</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="about">
+      <div class="main">
+        <img src="" alt="" />
+        <div class="about-text"></div>
+      </div>
+    </section>
